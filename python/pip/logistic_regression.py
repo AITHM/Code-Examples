6f2c149d715d04b04a4b0ac94ab64b0dbfb70af5
Alec Henderson <jc942017@ad.jcu.edu.au>
2022-11-25 11:54:40 +1000

    check action

--- python/pip/logistic_regression.py
+++ python/pip/logistic_regression.py
@@ -2,16 +2,17 @@ from sklearn.linear_model import LogisticRegression
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+
 def run_logistic_regression_iris():
     data = pd.read_csv("iris.csv")
-    x = data.loc[:,"SepalLengthCm":"PetalWidthCm"]
-    y = data.loc[:,"Species"]
+    x = data.loc[:, "SepalLengthCm":"PetalWidthCm"]
+    y = data.loc[:, "Species"]
 
     le = preprocessing.LabelEncoder()
     le.fit(y)
     y = le.transform(y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
     model = LogisticRegression().fit(x_train, y_train)
     return model.score(x_test, y_test)
